c05a26e0077d9669248cf1616cdeef3c142af2c5
Bogdan <jackfresco.pol@gmail.com>
2020-11-18 15:47:36 +0600

    first commit with less

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet/less" href="style.less" />
+	
+	<script src="less.js-2.0.0\dist\less.min.js"></script>
+	<title></title>
+
+	
+	<title></title>
+</head>
+<body>
+	hello git! hello loggerheead
+</body>
+</html>
