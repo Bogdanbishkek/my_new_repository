6d088156ea61eefbc4f138fd2e39c2ae233ac8c3
Bogdan <jackfresco.pol@gmail.com>
2020-12-08 18:00:16 +0600

    first lesson of practise make site done

--- index.php
+++ index.php
@@ -8,10 +8,30 @@
 	<title></title>
 </head>
 <body>
-	<div class="body-upper"></div>
-	<div class="body-container"></div>
-	<div class="body-footer">
-		<div class="container"></div>
+	<div id="upper"></div>
+	<div class="container">
+		<div class="header">
+			<div  class="header-clearFix">
+				<div class="header-clearFix-logo">
+
+			    </div>
+
+			    <form  class="header-clearFix-search">
+			    	<input placeholder="Search" name="">
+			    	<button>GO</button>
+			    </form>
+			</div>
+
+			<div class="primary-menu">
+				
+			</div>
+		</div>
+	</div>
+
+	<div class="footer">
+		<div class="container">
+			
+		</div>
 	</div>
 </body>
 </html>	
