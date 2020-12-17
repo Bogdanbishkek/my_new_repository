fd7835ac3c7afc96c3058541e9e08bd06e5afd59
Bogdan <jackfresco.pol@gmail.com>
2020-12-17 13:49:10 +0600

    done psd layout

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<link rel="stylesheet" href="less/style.css" />
 
-	 <script src="less.min.js"></script> 
+	 <script src="less.min.js"></script>
 	<title></title>
 	<title></title>
 </head>
@@ -24,7 +24,7 @@
 			</div>
 
 			<div class="primary-menu">
-				<div class="primary-menu-sectionActive"> 
+				<div class="primary-menu-sectionActive">
 					<a href="">home</a>
 				</div>
 				<div class="primary-menu-sectionUnactive">
@@ -53,36 +53,181 @@
 
 		<div class="blockBanner">
 			<div class="blockBanner-banner">
-			   <h1>Fucing banner</h1>
-			   <p>And some text below</p>
+			   <img src="../pictures/background/backgroundBanner.png">
+			   <div class="blockBanner-banner-sliderText">
+			   		<p class="blockBanner-banner-sliderText-bigText">Fucing bunner</p>
+			   		<p class="blockBanner-banner-sliderText-smalText">Small text</p>
+			   </div>
 		    </div>
-		    
+
 		    <ul class="blockBanner-panel">
 		    	<li class="blockBanner-panel-itemUnActive">
-		    
-		    		<a href="#"><h1>1</h1><h2>dada</h2></a>
+
+		    		<a href="#">dada</a>
 		    	</li>
-		    	<li class="blockBanner-panel-itemActive">
-		    
-		    		<a href="#"><h1>1</h1><h2>dada</h2></a>
+		    	<li class="blockBanner-panel-itemUnActive">
+
+		    		<a href="#">dada</a>
+		    	</li>
+		    	<li class="blockBanner-panel-itemUnActive">
+
+		    		<a href="#">dada</a>
 		    	</li>
 		    	<li class="blockBanner-panel-itemActive">
 
-		    		<a href="#"><h1>1</h1><h2>dada</h2></a>
-		    	</li>
-		    	<li class="blockBanner-panel-itemActive">
-	
-		    		<a href="#"><h1>1</h1><h2>dada</h2></a>
+		    		<a href="#">dada</a>
 		    	</li>
 		   </ul>
 		</div>
 
+		<div class="mainContent">
+			<div class="line1">
+				<div class="line1-box">
+					<p class="line1-box-header">
+						<span>title</span>
+					</p>
+					<img class="line1-box-picture" src="1luB.gif">
+					<p class="line1-box-text">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+					<a class="line1-box-link" href="">link</a>
+				</div>
+				<div class="line1-box">
+					<p class="line1-box-header">
+						<span>title</span>
+					</p>
+					<img class="line1-box-picture" src="1luB.gif">
+					<p class="line1-box-text">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+
+					<a class="line1-box-link" href="">link</a>
+				</div>
+			</div>
+
+			<div class="line2">
+				<div class="line2-box">
+					<p class="line2-box-header">
+						<span>title</span>
+					</p>
+
+					<p class="line2-box-text">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+
+					<a class="line1-box-link" href="">link</a>
+				</div>
+
+				<div class="line2-box">
+					<p class="line2-box-header">
+						<span>title</span>
+					</p>
+
+					<div class="users">
+						<div class="users-user">
+							<img class="users-user-img" src="1luB.gif">
+							<p class="users-user-name">name</p>
+							<p class="users-user-info">
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+							</p>
+						</div>
+						<div class="users-user">
+							<img class="users-user-img" src="1luB.gif">
+							<p class="users-user-name">name</p>
+							<p class="users-user-info">
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+							</p>
+						</div>
+						<div class="users-user">
+							<img class="users-user-img" src="1luB.gif">
+							<p class="users-user-name">name</p>
+							<p class="users-user-info">
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+							</p>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="line2-box">
+					<p class="line2-box-header">
+						<span>title</span>
+					</p>
+
+					<p class="line2-box-text">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+
+					<a class="line2-box-link" href="">link</a>
+				</div>
+			</div>
+			<div class="line3">
+				<div class="line3-box">
+					<p class="line1-box-header">
+						<span>title</span>
+					</p>
+				</div>
+			</div>
+		</div>
 	</div>
 
 	<div class="footer">
 		<div class="container">
-			
+			<div class="container-logo1">
+				<div class="container-logo1-box1">
+					<p class="container-logo1-box1-header">twitter free</p>
+					<a class="container-logo1-box1-link" href="">23 oct</a>
+					<p class="container-logo1-box1-text">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsu
+					</p>
+				</div>
+				<div class="container-logo1-box2">
+					<p class="container-logo1-box2-header">sitemap</p>
+					<ul class="container-logo1-box2-menu">
+						<li class="container-logo1-box2-menu-local"><a href="">home</a></li>
+						<li class="container-logo1-box2-menu-local"><a href="">home</a></li>
+						<li class="container-logo1-box2-menu-local"><a href="">home</a></li>
+						<li class="container-logo1-box2-menu-local"><a href="">home</a></li>
+						<li class="container-logo1-box2-menu-local"><a href="">home</a></li>
+						<li class="container-logo1-box2-menu-local"><a href="">home</a></li>
+					</ul>
+				</div>
+				<div class="container-logo1-box3">
+					<p class="container-logo1-box3-header">social network</p>
+					<ul class="container-logo1-box3-bigIcons">
+						<li class="container-logo1-box3-bigIcons-icon telegram">
+							<a href="https://twitter.com/?lang=ru"></a>
+						</li>
+						<li class="container-logo1-box3-bigIcons-icon facebook">
+							<a href="https://www.facebook.com/"></a>
+						</li>
+						<li class="container-logo1-box3-bigIcons-icon google">
+							<a href="https://www.google.ru/"></a>
+						</li>
+					</ul>
+					<ul class="container-logo1-box3-smallIcons">
+						<li class="container-logo1-box3-smallIcons-icon v">
+							<a href=""></a>
+						</li>
+						<li class="container-logo1-box3-smallIcons-icon youtube">
+							<a href=""></a>
+						</li>
+						<li class="container-logo1-box3-smallIcons-icon balloons">
+							<a href=""></a>	
+						</li>
+						<li class="container-logo1-box3-smallIcons-icon cannon">
+							<a href=""></a>
+						</li>
+						<li class="container-logo1-box3-smallIcons-icon antenna">
+							<a href=""></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="container-logo2">
+				<p>Copyright © 2012 Whitesquare. A <a href="">pcklab</a> creation</p>
+			</div>
 		</div>
 	</div>
 </body>
-</html>	
+</html>
